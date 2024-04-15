9d842cb84992b62233d796b52be953ca9dcc8b7c
Alex Salmeron <juanasalmeron95@gmail.com>
2024-04-15 09:02:13 -0400

    first commit, just ensuring local repo is connected to remote repo

---
+++ assignment1-decisions-at-the-crossroads.py
@@ -0,0 +1,2 @@
+#Task 1: Code Correction
+#You are provided with a Python script that uses conditional statements to tell if a number is positive, negative, or zero, but it has some errors. Identify and fix them.
